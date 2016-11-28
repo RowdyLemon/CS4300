731086f95a01b7f1c7f20dac0236ce19dd481158
Matt Lemon <u0575787@utah.edu>
2016-11-28 14:30:26 -0700

    everything good

--- A8/CS4300_MDP_policy.m
+++ A8/CS4300_MDP_policy.m
@@ -32,46 +32,4 @@ for i=1:length(U)
         policy(i) = U(i);
     end
 end
-
-% x = 4;
-% y = length(policy) / x;
-% 
-% for i=1:length(policy)
-%     xpos = floor(mod(i,x)) + ((mod(i, x) == 0) * x);
-%     ypos = floor(i/x) + (mod(i, x) > 0);
-%     
-%     right = (xpos+1) + (ypos-1)*4;
-%     left = (xpos-1) + (ypos-1)*4;
-%     up = xpos + (ypos)*4;
-%     down = xpos + (ypos-2)*4;
-%     
-%     action = 0;
-%     value = 0;
-%     if(ypos < y)
-%        value = U(up);
-%        action = 1;
-%     end
-%     if(xpos > 1)
-%         if(value < U(left))
-%            value = U(left);
-%            action = 2;
-%         end
-%     end
-%     if(ypos > 1)
-%         if(value < U(down))
-%             value = U(down);
-%             action = 3;
-%         end
-%     end
-%     if(xpos < x)
-%         if(value < U(right))
-%             action = 4;
-%         end
-%     end
-%     
-%     if((U(12) == 1 && (U(i) == 0 || U(i) == -1 || U(i) == 1)) || U(i) == 1000 || U(i) == -1000)
-%         policy(i) = U(i);
-%     else
-%         policy(i) = action;
-%     end
-% end
+end
